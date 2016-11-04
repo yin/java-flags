181dce80ed340c4676be05dede9bbed4acd220d7
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2016-11-05 00:23:30 +0100

    fixed release script

--- scripts/maven_sonatype_repo_release.sh
+++ scripts/maven_sonatype_repo_release.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-if git branch | grep '* master' &> /dev/null; then
+if ! git branch | grep '* master' &> /dev/null; then
     echo "checkout master first"
     exit 1
 fi
